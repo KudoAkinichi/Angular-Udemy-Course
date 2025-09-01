b03f73d2aab3ac285d4d98da5354848d9588bc70
KudoAkinichi <arookrishna@gmail.com>
2025-09-01 05:42:46 +0530

    Accepting Objects As Inputs & Adding Appropriate Typings

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -23,9 +23,15 @@ export class UserComponent {
   // selectedUser = DUMMY_USERS[randomIndex];
   // selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+
+  @Input({ required: true }) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  };
   @Output() select = new EventEmitter<string>();
   // select = output<string>();
 
@@ -45,13 +51,13 @@ export class UserComponent {
   // }
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
     // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     // this.selectedUser.set(DUMMY_USERS[randomIndex]);
     // // this.selectedUser = DUMMY_USERS[randomIndex];
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
