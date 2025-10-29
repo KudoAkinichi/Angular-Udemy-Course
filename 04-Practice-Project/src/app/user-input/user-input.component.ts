46b1038c5af359f4eab63a3c56a5ba912de4dcb3
KudoAkinichi <arookrishna@gmail.com>
2025-10-29 11:23:05 +0530

    Practice Project S4 | V74-79 | Header + UserInput + Form + 2-way bind (standalone + no signals usage)

---
+++ 04-Practice-Project/src/app/user-input/user-input.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrls: ['./user-input.component.css'],
+})
+export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  onSubmit() {
+    console.log('submit');
+    console.log(this.enteredInitialInvestment);
+    console.log(this.enteredAnnualInvestment);
+    console.log(this.enteredExpectedReturn);
+    console.log(this.enteredDuration);
+  }
+}
